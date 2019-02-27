973a3dcd7b963fc28fee0509c2a58ea7512438eb
Juan Diego Alonso <tontolanona@gmail.com>
2019-02-27 18:16:59 +0100

    Inicial

---
+++ sumador/pruebas/sumador/TestASumar.java
@@ -0,0 +1,14 @@
+package sumador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestASumar {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
