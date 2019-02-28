6fad687f71d2826ec121c56e966e3eb5bb38d467
Juan Diego Alonso <tontolanona@gmail.com>
2019-02-28 19:25:35 +0100

    Añadido con valor mayor que 9

--- sumador/pruebas/sumador/TestASumar.java
+++ sumador/pruebas/sumador/TestASumar.java
@@ -8,9 +8,19 @@ public class TestASumar {
 
 	@Test
 	public void testUnaCifra() {
-        ASumar sumi = new ASumar("-17785");
+        ASumar sumi = new ASumar("7");
+        String resultado = sumi.mostrar();
+        assertEquals("7 = 7",resultado );
+    }	@Test
+	public void testNegativo() {
+        ASumar sumi = new ASumar("-18785");
         String resultado = sumi.mostrar();
         assertEquals("",resultado );
+    }	@Test
+	public void testMayorNueve() {
+        ASumar sumi = new ASumar("17785");
+        String resultado = sumi.mostrar();
+        assertEquals("1+7+7+8+5 = 28",resultado );
     }
 
 }
--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -7,10 +7,24 @@ public class ASumar {
 	public ASumar(String string) { // Este método almacena la variable que le introducimos
 		this.valor_inicial = string;
 	}
+	public String total() {// Este método calcula  la suma y la cadena del resultado
+        int suma = 0;
+        String resultado ="";
+        String numero = valor_inicial;
+        for (int i = 0; i < numero.length(); i++) {
+            String digito = numero.substring(i,i+1);
+            suma += Integer.parseInt(digito);
+            if (i < 1) {
+            	resultado += digito;
+            }else {
+            	resultado += "+"+digito;
+            }
+        }
+        return (resultado + " = " + suma);
 
+    }
 	public String mostrar() { //Este método muestra el resultado a partir de la variable introducida
 		 String numero = valor_inicial;
-		 int suma = 0;
 		 if (numero.length() == 1) {
 	            return (numero + " = " + numero);
 		}
@@ -18,6 +32,6 @@ public class ASumar {
 			 return ("");
 		}
 	        
-	      else return ("Introduzca un número valido");
+	      else 	 return (total());
 	}
 }
